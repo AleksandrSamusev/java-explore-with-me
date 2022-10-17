9ac5eeab1f47151a93b0c48a22aefe8d758e6846
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2022-10-17 13:09:51 +0300

    feat: add partially application structure

---
+++ gen/src/main/java/ru/practicum/ewm/ExploreWithMeGeneral.java
@@ -0,0 +1,11 @@
+package ru.practicum.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeGeneral {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMeGeneral.class, args);
+    }
+}
