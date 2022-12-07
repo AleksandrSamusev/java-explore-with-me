3d1c6ed02896d4a41ce7bf148be49763a60d2a84
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2022-12-07 20:05:28 +0300

    fix: fix get stacktrace in handler

--- mainapp/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
+++ mainapp/src/main/java/ru/practicum/ewm/exception/ErrorHandler.java
@@ -5,17 +5,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collections;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
+    StringWriter sw = new StringWriter();
+    PrintWriter pw = new PrintWriter(sw);
+    ApiError apiError = new ApiError();
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleUserNotFoundException(final UserNotFoundException e) {
-        ApiError apiError = new ApiError();
         Arrays.stream(e.getStackTrace())
                 .forEach(er -> apiError.getErrors().add(er.toString()));
         apiError.setStatus(HttpStatus.NOT_FOUND.name());
@@ -28,9 +34,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleCategoryConflictException(final CategoryConflictException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.CONFLICT.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Conflict");
@@ -41,9 +47,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleUserConflictException(final UserConflictException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.CONFLICT.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Conflict");
@@ -54,9 +60,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleCategoryNotFoundException(final CategoryNotFoundException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.NOT_FOUND.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Not found");
@@ -67,9 +73,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleEventNotFoundException(final EventNotFoundException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.NOT_FOUND.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Not found");
@@ -80,9 +86,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleRequestNotFoundException(final RequestNotFoundException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.NOT_FOUND.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Not found");
@@ -93,9 +99,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleCompilationNotFoundException(final CompilationNotFoundException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.NOT_FOUND.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Not found");
@@ -106,9 +112,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ApiError handleInvalidParameterException(final InvalidParameterException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.BAD_REQUEST.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Bad request");
@@ -119,9 +125,9 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiError handleReviewNotFoundException(final ReviewNotFoundException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.NOT_FOUND.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Not found");
@@ -132,13 +138,14 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ApiError handleForbiddenException(final ForbiddenException e) {
-        ApiError apiError = new ApiError();
-        Arrays.stream(e.getStackTrace())
-                .forEach(er -> apiError.getErrors().add(er.toString()));
+        e.printStackTrace(pw);
+        String stackTrace = sw.toString();
+        apiError.setErrors(Collections.singletonList(stackTrace));
         apiError.setStatus(HttpStatus.FORBIDDEN.name());
         apiError.setMessage(e.getMessage());
         apiError.setReason("Forbidden");
         apiError.setTimestamp(Timestamp.from(Instant.now()));
         return apiError;
     }
+
 }
